8712dcfc9b6edd0762085751bc544c75452a098a
devid <nnick4673@gmail.com>
2023-03-12 18:09:16 -0300

    Funcionalidade de resumir ativos não depende mais que o ativo possua todos os indicadores de uma ação por exemplo, depende apenas dos indicadores que o ativo possua

--- projeto_lena/main.py
+++ projeto_lena/main.py
@@ -1,6 +1,5 @@
-from util import formata_numero, porcent
+from util import formata_numero, formata_porcent, formata_preco
 from typer import Typer
-from rich import print
 from rich.console import Console
 from rich.table import Table
 import yahooquery as yq
@@ -13,46 +12,36 @@ def resumo(ticket: str) -> None:
 		Este comando recebe como argumento o ticket de alguma empresa da B3 e
 		retorna uma série de indicadores sobre tal ativo.
 	"""
+	ticket += '.SA'
 	dados = yq.Ticker(ticket)
-	preco_atual = dados.financial_data[ticket]['currentPrice']
-	lucro_bruto = dados.financial_data[ticket]['grossProfits']
-	receita_total = dados.financial_data[ticket]['totalRevenue']
-	endividamento = dados.financial_data[ticket]['totalDebt']
-	fluxo_de_caixa_total = dados.financial_data[ticket]['totalCash']
-	fluxo_de_caixa_op = dados.financial_data[ticket]['operatingCashflow']
-	fluxo_de_caixa_livre = dados.financial_data[ticket]['operatingCashflow']
-	ebitda = dados.financial_data[ticket]['ebitda']
-	retorno_sobre_PL = dados.financial_data[ticket]['returnOnEquity']
-	retorno_sobre_ativos = dados.financial_data[ticket]['returnOnAssets']
-	margem_operacional = dados.financial_data[ticket]['operatingMargins']
-	margem_de_lucro = dados.financial_data[ticket]['profitMargins']
-	margem_ebitda = dados.financial_data[ticket]['ebitdaMargins']
-	crescimento_de_receita = dados.financial_data[ticket]['revenueGrowth']
+	chaves_dados = dados.financial_data[ticket].keys()
+	indicadores = {
+		'currentPrice': ('Preço atual', formata_preco),
+		'grossProfits': ('Lucro Bruto', formata_numero),
+		'totalRevenue': ('Receita Total', formata_numero),
+		'totalDebt': ('Endividamento', formata_numero),
+		'totalCash': ('Fluxo de Caixa Total', formata_numero),
+		'operatingCashflow': ('Fluxo de Caixa Operacional', formata_numero),
+		'EBITDA': ('EBTIDA', formata_numero),
+		'returnOnEquity': ('Retorno sobre PL', formata_porcent),
+		'returnOnAssets': ('Retorno sobre Ativos', formata_porcent),
+		'operatingMargins': ('Margem Operacional', formata_porcent),
+		'profitMargins': ('Margem de Lucro', formata_porcent),
+		'ebitdaMargins': ('Margem do EBITDA', formata_porcent),
+		'revenueGrowth': ('Crescimento da Receita', formata_porcent)
+	}
 
 	tabela = Table(title = f'{ticket}')
 	tabela.add_column('Indicadores', justify = 'center')
 	tabela.add_column('Valores', justify = 'center')
 
-	tabela.add_row('Preço Atual', f'R${preco_atual:.2f}')
-	tabela.add_row('Lucro Bruto', formata_numero(lucro_bruto))
-	tabela.add_row('Receita Total', formata_numero(receita_total))
-	tabela.add_row('Endividamento Total', formata_numero(endividamento))
-	tabela.add_row('Fluxo de Caixa Total', formata_numero(fluxo_de_caixa_total))
-	tabela.add_row('Fluxo de Caixa Livre', formata_numero(fluxo_de_caixa_livre))
-	tabela.add_row('Fluxo de Caixa Operacional', formata_numero(fluxo_de_caixa_op))
-	tabela.add_row('EBITDA', formata_numero(ebitda))
-	tabela.add_row('Retorno Sobre PL', porcent(retorno_sobre_PL))
-	tabela.add_row('Retorno Sobre Ativos', porcent(retorno_sobre_ativos))
-	tabela.add_row('Margem Operacional', porcent(margem_operacional))
-	tabela.add_row('Margem de Lucro', porcent(margem_de_lucro))
-	tabela.add_row('Margem EBITDA', porcent(margem_ebitda))
-	tabela.add_row('Crescimento de Receita', porcent(crescimento_de_receita))
+	for indic in indicadores:
+		if indic in chaves_dados:
+			indic_fmt = indicadores[indic][0]
+			funcao_fmt = indicadores[indic][1]
+			tabela.add_row(f'{indic_fmt}', funcao_fmt(dados.financial_data[ticket][indic]))
 
 	console = Console()
 	console.print(tabela)
-
-@app.command()
-def comando() -> None:
-	pass
 
 app()
